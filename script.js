555ebcda8786fcaf47cc1d5ba342717a367b0c40
anet-eny <anet.eny@gmail.com>
2024-03-03 13:12:35 +0100

    getComputerChoice function created

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("hi")
+let choices = ["Rock", "Paper", "Scissors"]
+
+let getComputerChoice = function(){
+    let computerChoice = Math.floor(Math.random() * 3)
+    return choices[computerChoice]
+}
+
+let result = getComputerChoice()
+console.log(result)
