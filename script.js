5e4e3f2e59d29f4924f41bf7f5147ee8c5d7ec7b
anet-eny <anet.eny@gmail.com>
2024-03-03 13:48:28 +0100

    playRound function created, all conditions added and functionality verified

--- script.js
+++ script.js
@@ -5,11 +5,31 @@ let getComputerChoice = function(){
     return choices[computerChoice]
 }
 
-let computerSelection = getComputerChoice()
-console.log(computerSelection)
+// let computerSelection = getComputerChoice()
+// console.log(computerSelection) 
 
 let playRound = function(playerSelection, computerSelection) {
     let playerSelection2 = playerSelection.toLowerCase()
-    
-
+    if(playerSelection2 === "rock" && computerSelection === "rock"){
+        return "tie"
+    } else if(playerSelection2 === "paper" && computerSelection === "paper"){
+        return "tie"
+    } else if(playerSelection2 === "scissors" && computerSelection === "scissors"){
+        return "tie"
+    } else if(playerSelection2 === "rock" && computerSelection === "paper"){
+        return "computer wins"
+    } else if(playerSelection2 === "rock" && computerSelection === "scissors"){
+        return "player wins"
+    } else if(playerSelection2 === "paper" && computerSelection === "rock"){
+        return "player wins"
+    } else if(playerSelection2 === "paper" && computerSelection === "scissors"){
+        return "computer wins"
+    } else if(playerSelection2 === "scissors" && computerSelection === "rock"){
+        return "computer wins"
+    } else if(playerSelection2 === "scissors" && computerSelection === "paper"){
+        return "player wins"
+    }
 }
+const playerSelection = "rock"
+const computerSelection = getComputerChoice()
+console.log(playRound(playerSelection, computerSelection))
