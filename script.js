60f2d71817615ba1317d6783d98e962f2ab8d5a3
anet-eny <anet.eny@gmail.com>
2024-03-26 20:43:58 +0100

    Update click functions: new const result, activation of playRound() and counting scores

--- script.js
+++ script.js
@@ -2,11 +2,9 @@ let choices = ["rock", "paper", "scissors"]
 
 
 
-let playerChoice = ""
-
-let playRound = function() {
+let playRound = function(playerSelection) {
     const computerSelection = choices[Math.floor(Math.random() * 3)]
-    let playerSelection = playerChoice
+    
     
     const pComputer = document.querySelector(".h1-computer p")
     if (!pComputer){
@@ -16,9 +14,9 @@ let playRound = function() {
     pComputer.textContent = computerSelection
 
     
-    let pPlayer = document.querySelector(".h1-player p")
+    const pPlayer = document.querySelector(".h1-player p")
     if (!pPlayer){
-        pPlayer = document.createElement("p")
+        const pPlayer = document.createElement("p")
         document.querySelector(".h1-player").appendChild(pPlayer)
     }
     pPlayer.textContent = playerSelection
@@ -38,75 +36,84 @@ let playRound = function() {
     
 }
 
-let result = document.querySelector(".results p")
-    if (!result){
-        result = document.createElement("p")
-        document.querySelector(".results").appendChild(result)
-    } 
+// let result = document.querySelector(".results p")
+//     if (!result){
+//         result = document.createElement("p")
+//         document.querySelector(".results").appendChild(result)
+//     } 
 
-
-document.querySelector(".btnRock").addEventListener("click", function(){
-    playerChoice = "rock"
-    result.textContent = playRound()
-    playGame()
-})
-document.querySelector(".btnPaper").addEventListener("click", function(){
-    playerChoice = "paper"
-    result.textContent = playRound()
-    playGame()
-})
-document.querySelector(".btnScissors").addEventListener("click", function(){
-    playerChoice = "scissors"
-    result.textContent = playRound()
-    playGame()
-})
-   
 let scorePlayer = 0
 let scoreComputer = 0
 
-
-let playGame = function(){
-    
-    
-    const roundResult = playRound()
-    
-    if(roundResult === "player wins"){
+document.querySelector(".btnRock").addEventListener("click", function(){
+    const result = playRound("rock")
+    if(result === "player wins") {
         scorePlayer++
-    } else if(roundResult === "computer wins"){
+    } else if(result === "computer wins") {
         scoreComputer++
     }
-    console.log(roundResult)
-
-    let score = document.querySelector(".score p")
-    if (!score){
-        score = document.createElement("p")
-        document.querySelector(".score").appendChild(score)
-    } 
-    score.textContent = `Player: ${scorePlayer}/5 Computer: ${scoreComputer}/5 `
-
-    let gameResult = document.querySelector("score p.game-result")
-    if (!gameResult){
-        gameResult = document.createElement("p")
-        gameResult.classList.add("game-result")
-        document.querySelector(".score").appendChild(gameResult)
+})
+document.querySelector(".btnPaper").addEventListener("click", function(){
+    const result = playRound("paper")
+    if(result === "player wins") {
+        scorePlayer++
+    } else if(result === "computer wins") {
+        scoreComputer++
     }
-
-    if(scorePlayer === 5){
-        console.log("player wins the game")
-        gameResult.textContent = "Player wins the game"
-        scorePlayer = 0
-        scoreComputer = 0
-        
-
-    } else if(scoreComputer === 5){
-        console.log("computer wins the game")
-        gameResult.textContent = "Computer wins the game"
-        scorePlayer = 0
-        scoreComputer = 0
-        
-    } else {
-        gameResult.textContent = ""
+})
+document.querySelector(".btnScissors").addEventListener("click", function(){
+    const result = playRound("scissors")
+    if(result === "player wins") {
+        scorePlayer++
+    } else if(result === "computer wins") {
+        scoreComputer++
     }
+})
+   
+
+
+// let playGame = function(){
     
-}
+    
+//     const roundResult = playRound()
+    
+//     if(roundResult === "player wins"){
+//         scorePlayer++
+//     } else if(roundResult === "computer wins"){
+//         scoreComputer++
+//     }
+//     console.log(roundResult)
+
+//     let score = document.querySelector(".score p")
+//     if (!score){
+//         score = document.createElement("p")
+//         document.querySelector(".score").appendChild(score)
+//     } 
+//     score.textContent = `Player: ${scorePlayer}/5 Computer: ${scoreComputer}/5 `
+
+//     let gameResult = document.querySelector("score p.game-result")
+//     if (!gameResult){
+//         gameResult = document.createElement("p")
+//         gameResult.classList.add("game-result")
+//         document.querySelector(".score").appendChild(gameResult)
+//     }
+
+//     if(scorePlayer === 5){
+//         console.log("player wins the game")
+//         gameResult.textContent = "Player wins the game"
+//         scorePlayer = 0
+//         scoreComputer = 0
+        
+
+//     } else if(scoreComputer === 5){
+//         console.log("computer wins the game")
+//         gameResult.textContent = "Computer wins the game"
+//         scorePlayer = 0
+//         scoreComputer = 0
+        
+//     } else {
+//         gameResult.textContent = ""
+//     }
+    
+// }
 
